cfd3c1702f4d3638455d9de78f1f70548e999fdf
Intizar Gul <41545797+intizarGul@users.noreply.github.com>
2020-05-24 19:15:31 +0100

    Toast Activity removed to be added alter on again

--- app/src/main/java/com/example/kotlinusercommunication/MainActivity.kt
+++ app/src/main/java/com/example/kotlinusercommunication/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.kotlinusercommunication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.btnLaunchToast).setOnClickListener(this)
+        findViewById<View>(R.id.btnLaunchSnackbar).setOnClickListener(this)
+        findViewById<View>(R.id.btnLaunchDialog).setOnClickListener(this)
+        findViewById<View>(R.id.btnLaunchNotification).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            //R.id.btnLaunchToast -> startActivity(Intent(this, ToastActivity::class.java))
+            //R.id.btnLaunchSnackbar -> startActivity(Intent(this, SnackbarActivity::class.java))
+            //R.id.btnLaunchDialog -> startActivity(Intent(this, DialogActivity::class.java))
+            //R.id.btnLaunchNotification -> startActivity(Intent(this, NotificationActivity::class.java))
+        }
     }
 }
